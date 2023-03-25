b6e0d80c12492993435f5dfcd7b71376446b3a2c
Beau Rubin-Rottenberg <roscoe.rr@icloud.com>
2023-03-24 21:06:52 -0400

    page link fixes and email

--- RSVP/script.js
+++ RSVP/script.js
@@ -1,3 +1,9 @@
+function switchToRSVP() {
+    window.location.href = "RSVP/index.html"
+}
 function switchTab() {
     window.location.href = "../index.html"
+}
+function  email() {
+    window.location.href = "mailto:jrotten@earstudio.com"
 }
